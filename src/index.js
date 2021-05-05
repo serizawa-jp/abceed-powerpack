7b467921f5feb8492339780485ac512e7fbe41a5
SERIZAWA <serizawa@hey.com>
2021-05-05 09:16:50 +0900

    support confirmation test

--- src/index.js
+++ src/index.js
@@ -4,55 +4,107 @@
     const config = {
         showCommentaryKey: {
             key: "space",
-            cb: () => clickCommentaryButton(),
-            getElem: () => document.querySelector(".commentary-button"),
+            cb: () => {
+                clickCommentaryButton();
+                clickConfirmationTestButton();
+            },
+            getElems: () => document.querySelectorAll(".commentary-button,.bottom-navs__long-nav"),
         },
         pressKnownButtonKey: {
             key: "k",
-            cb: () => clickKnownButton(),
-            getElem: () => document.querySelector(".choice-button.is-known"),
+            cb: () => {
+                clickKnownButton();
+                clickNextButton();
+            },
+            getElems: () => document.querySelectorAll(".choice-button.is-known,.bottom-navs__short-nav--right a"),
         },
         pressUnknownButtonKey: {
             key: "j",
-            cb: () => clickUnknownButton(),
-            getElem: () => document.querySelector(".choice-button.is-unknown"),
+            cb: () => {
+                clickUnknownButton();
+                clickAgainButton();
+            },
+            getElems: () => document.querySelectorAll(".choice-button.is-unknown,.bottom-navs__short-nav--left a"),
         },
         pressExampleButtonKey: {
             key: "1",
-            cb: () => switchToExample(),
-            getElem: () => getModeElem("例文"),
+            cb: () => {
+                switchToExample();
+                switchEnToJa();
+            },
+            getElems: () => [getModeElem("例文"), getModeElem("英→日")],
         },
         pressWordButtonKey: {
             key: "2",
-            cb: () => switchToWord(),
-            getElem: () => getModeElem("単語"),
+            cb: () => {
+                switchToWord();
+                switchJaToEn();
+            },
+            getElems: () => [getModeElem("単語"), getModeElem("日→英")],
         },
         pressExampleJaButtonKey: {
             key: "3",
-            cb: () => switchToExampleJa(),
-            getElem: () => getModeElem("例文訳"),
+            cb: () => {
+                switchToExampleJa();
+                switchAudioToJa();
+            },
+            getElems: () => [getModeElem("例文訳"), getModeElem("音→日")],
         },
         pressWordJaButtonKey: {
             key: "4",
-            cb: () => switchToWordJa(),
-            getElem: () => getModeElem("単語訳"),
+            cb: () => {
+                switchToWordJa();
+                switchFillInTheBlank();
+            },
+            getElems: () => [getModeElem("単語訳"), getModeElem("穴埋め")],
         },
         playAudioKey: {
             key: "p",
             cb: () => playAudio(),
-            getElem: null,
+            getElems: null,
+        },
+        pressAnswer1: {
+            key: "a",
+            cb: () => clickAnswer(1),
+            getElems: () => [getAnswer(1)],
+        },
+        pressAnswer2: {
+            key: "s",
+            cb: () => clickAnswer(2),
+            getElems: () => [getAnswer(2)],
+        },
+        pressAnswer3: {
+            key: "d",
+            cb: () => clickAnswer(3),
+            getElems: () => [getAnswer(3)],
+        },
+        pressAnswer4: {
+            key: "f",
+            cb: () => clickAnswer(4),
+            getElems: () => [getAnswer(4)],
+        },
+        pressClose: {
+            key: "x",
+            cb: () => clickClose(),
+            getElems: () => document.querySelectorAll(".exam-header .exam-header-main>a"),
         },
     }
 
-    const addKeyLabel = (e, key) => e.textContent = `${e.textContent} [${key.toUpperCase()}]`;
+    const addKeyLabel = (e, key) => {
+        if (!e) return;
+
+        const t = e?.textContent;
+        if (t?.includes("[")) return;
+        e.textContent = `${t} [${key.toUpperCase()}]`;
+    }
     const getButtonOriginalLabel = (text) => text?.trim().split(" ")[0];
 
     const getChangeModeButtons = () =>
-        Array.from(document.querySelectorAll(".change-phrase-mode-btn a"));
+        Array.from(document.querySelectorAll(".change-phrase-mode-btn a,.change-mode-btns a"));
     const getModeElem = (mode) => {
         let e = null;
         getChangeModeButtons().forEach((b) => {
-            if (getButtonOriginalLabel(b.textContent) === mode) e = b;
+            if (getButtonOriginalLabel(b?.textContent) === mode) e = b;
         });
         return e;
     }
@@ -62,6 +114,10 @@
     const switchToExampleJa = () => switchMode("例文訳");
     const switchToWord = () => switchMode("単語");
     const switchToWordJa = () => switchMode("単語訳");
+    const switchEnToJa = () => switchMode("英→日");
+    const switchJaToEn = () => switchMode("日→英");
+    const switchAudioToJa = () => switchMode("音→日");
+    const switchFillInTheBlank = () => switchMode("穴埋め")
     const playAudio = () =>
         document.querySelector(".speaker-button-component")?.click();
     const clickCommentaryButton = () =>
@@ -70,6 +126,16 @@
         document.querySelector(".choice-button.is-known")?.click();
     const clickUnknownButton = () =>
         document.querySelector(".choice-button.is-unknown")?.click();
+    const clickConfirmationTestButton = () => document.querySelector(".bottom-navs__long-nav")?.click();
+    const clickNextButton = () => document.querySelector(".bottom-navs__short-nav--right a")?.click();
+    const clickAgainButton = () => document.querySelector(".bottom-navs__short-nav--left a")?.click();
+    const getAnswer = (n) => {
+        const a = Array.from(document.querySelectorAll(".selection-button-wrapper button"));
+        if (a.length < 4) return null;
+        return a[n - 1];
+    }
+    const clickAnswer = (n) => getAnswer(n)?.click();
+    const clickClose = () => document.querySelector(".exam-header .exam-header-main>a")?.click();
 
     const activate = () => {
         autoOpenDictionary();
@@ -90,7 +156,7 @@
     const enableKeyShortCut = () => {
         // add key event listener
         (() => {
-            const getElem = () => document.querySelector(".study-content-area");
+            const getElem = () => document.querySelector("body");
             if (getElem().dataset.enableKeyShortCut) return;
             getElem().dataset.enableKeyShortCut = "true";
 
@@ -102,13 +168,9 @@
 
         // add key label
         for (const [_, obj] of Object.entries(config)) {
-            const getElem = obj.getElem;
-            if (!getElem) continue;
-
-            const e = getElem();
-            if (!e || e.dataset.added) continue;
-            e.dataset.added = "true";
-            addKeyLabel(e, obj.key);
+            const getElems = obj.getElems;
+            if (!getElems) continue;
+            getElems().forEach(e => addKeyLabel(e, obj.key))
         }
     }
 
@@ -128,15 +190,19 @@
                     switch (idx) {
                         case 0:
                             clickKnownButton();
+                            clickAnswer(1);
                             break;
                         case 1:
                             clickCommentaryButton();
+                            clickAnswer(2);
                             break;
                         case 2:
                             playAudio();
+                            clickAnswer(3);
                             break;
                         case 3:
                             clickUnknownButton();
+                            clickAnswer(4);
                             break;
                         default:
                             console.log(`unknown button index: ${idx}`);
@@ -145,10 +211,22 @@
                 }
             });
 
-        if (gp.axes[0] === 1) switchToExample();
-        if (gp.axes[1] === 1) switchToWord();
-        if (gp.axes[0] === -1) switchToExampleJa();
-        if (gp.axes[1] === -1) switchToWordJa();
+        if (gp.axes[0] === 1) {
+            switchToExample();
+            switchEnToJa();
+        }
+        if (gp.axes[1] === 1) {
+            switchToWord();
+            switchJaToEn();
+        }
+        if (gp.axes[0] === -1) {
+            switchToExampleJa();
+            switchAudioToJa();
+        }
+        if (gp.axes[1] === -1) {
+            switchToWordJa();
+            switchFillInTheBlank();
+        }
     }, 100);
 
     const timeout = () => {
